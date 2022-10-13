d02069df04d35a98e6eb49dc34ab69a4aa59e838
nicho <kanwchan0@gmail.com>
2022-10-13 19:53:14 -0400

    Added title and comments that tell user what the program is

--- madLibs/madLibs/Program.cs
+++ madLibs/madLibs/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+/*
+      This program is a concole basesd mad lib generator
+      Author: Nichole
+      */
+
+
+      // Let the user know that the program is starting:
+      Console.WriteLine("Mad Libs has begun!");
+
+      // Give the Mad Lib a title:
+      string title = "A Mad lib day";
+
