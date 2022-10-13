1e0084a86c8fbc20d8ea72052fa715b2edb24ec4
nicho <kanwchan0@gmail.com>
2022-10-13 15:30:13 -0400

    Added first c# program printing to the console ascii art

---
+++ Console Creatures/Program.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine("⠀⠀⠀⠀⠀    ⠀⠀ ＿＿");    
+      Console.WriteLine("　　　　　／＞　　フ"); 
+      Console.WriteLine("　　　　　| 　_　 _ |"); 
+      Console.WriteLine("　 　　　／` ミ＿xノ"); 
+      Console.WriteLine("　　 　 /　　　 　 |"); 
+      Console.WriteLine("　　　 /　 ヽ　　 ﾉ"); 
+      Console.WriteLine("　 　 │　　|　|　|"); 
+      Console.WriteLine("　／￣|　　 |　|　|"); 
+      Console.WriteLine("　| (￣ヽ＿_ヽ_)__)"); 
+      Console.WriteLine("　＼二つ"); 
+      Console.WriteLine("Cute cat ascii art");
+     }
+  }
+}
