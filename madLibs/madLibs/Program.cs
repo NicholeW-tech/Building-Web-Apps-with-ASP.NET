a6b06860fad7513c80124d7918e74d0f1b8d87bc
nicho <kanwchan0@gmail.com>
2022-10-13 20:04:48 -0400

    Added variables and user input lines for mad lib generator

--- madLibs/madLibs/Program.cs
+++ madLibs/madLibs/Program.cs
@@ -1,13 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
 /*
-      This program is a concole basesd mad lib generator
+      This program is a console based mad lib generator
       Author: Nichole
       */
 
 
-      // Let the user know that the program is starting:
-      Console.WriteLine("Mad Libs has begun!");
+// Let the user know that the program is starting:
+Console.WriteLine("Mad Libs has begun!");
 
-      // Give the Mad Lib a title:
-      string title = "A Mad lib day";
-
+// Give the Mad Lib a title:
+string title = "A Mad lib day";
+Console.WriteLine(title);
+// Define user input and variables:
+Console.Write("Enter a name: ");
+string name = Console.ReadLine();
+Console.Write("Enter an adjective: ");
+string adj1 = Console.ReadLine();
+Console.Write("Enter an adjective: ");
+string adj2 = Console.ReadLine();
+Console.Write("Enter an adjective: ");
+string adj3 = Console.ReadLine();
+Console.Write("Enter a verb: ");
+string verb1 = Console.ReadLine();
+Console.Write("Enter a noun: ");
+string noun1 = Console.ReadLine();
+Console.Write("Enter a noun: ");
+string noun2 = Console.ReadLine();
+Console.Write("Enter an animal: ");
+string animal = Console.ReadLine();
+Console.Write("Enter a food: ");
+string food = Console.ReadLine();
+Console.Write("Enter a fruit: ");
+string fruit = Console.ReadLine();
+Console.Write("Enter a superhero: ");
+string superhero = Console.ReadLine();
+Console.Write("Enter a country: ");
+string country = Console.ReadLine();
+Console.Write("Enter a dessert: ");
+string dessert = Console.ReadLine();
+Console.Write("Enter a year: ");
+string year = Console.ReadLine();
